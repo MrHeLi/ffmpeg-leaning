4ef2c1c0fcf82244b3ecbe3e0bc1834e2e555560
kiven <superliunique@gmail.com>
2019-05-15 09:03:45 +0800

    修改部分目录

---
+++ play_ffmpeg/play_ffmpeg_show_video/PlayFFmpeg/cmake-build-debug/CMakeFiles/PlayFFmpeg.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/heli/CLionProjects/PlayFFmpeg/main.cpp" "/Users/heli/CLionProjects/PlayFFmpeg/cmake-build-debug/CMakeFiles/PlayFFmpeg.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/ffmpeg/4.1.3_1/include"
+  "/usr/local/Cellar/sdl2/2.0.9_1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ play_ffmpeg/play_ffmpeg_show_video/PlayFFmpeg/cmake-build-debug/CMakeFiles/PlayFFmpeg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PlayFFmpeg.dir/main.cpp.o"
+  "PlayFFmpeg.pdb"
+  "PlayFFmpeg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PlayFFmpeg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
